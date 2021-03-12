3c9bb3899f007b810ed196fc81bc8ca172f5f1c1
Dylan Strohschein <dylanstrohschein@gmail.com>
2021-03-11 21:46:26 -0500

    10 levels with win state

--- src/entities/weapons/MachineGun.cpp
+++ src/entities/weapons/MachineGun.cpp
@@ -2,7 +2,7 @@
 
 MachineGun::MachineGun(Scene* scene, bool enemy) : BaseWeapon(scene, enemy){
     // Make the adjustments relative to the base gun
-    fireRate = fireRate / 3;
+    fireRate = fireRate / 2;
     shootSpeed = shootSpeed * 1.2;
     damage = 1;
 }
